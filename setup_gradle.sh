fad28a9bfc990d4f170b4cbabc464f1c5568d3b1
bspkrs <bspkrs@gmail.com>
2015-11-16 10:05:47 -0500

    update scripts for FG2

--- setup_gradle.sh
+++ setup_gradle.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
+# usage: ./setup_gradle.sh 2.0 1.8 forge promos/1.8-latest stable_17
 
 FG=$1
 PLUGIN=$3
 MC_VERSION=$2
 PLUGIN_VERSION=`python get_json_value.py ${PLUGIN} $4`
-VERSION=${MC_VERSION}-${PLUGIN_VERSION}-${MC_VERSION}
+VERSION=${MC_VERSION}-${PLUGIN_VERSION}
 MAPPINGS=$5
 
 # files
 TO_REPLACE=build.gradle_pre
 GRADLE_BUILD=build.gradle
 
-# delete old buiold.gradle, and copy in new one.
+# delete old build.gradle, and copy in new one.
 rm -rf ${GRADLE_BUILD}
 cp ${TO_REPLACE} ${GRADLE_BUILD}
 
@@ -20,4 +21,4 @@ sed -i 's/@PLUGIN@/'${PLUGIN}'/g' ${GRADLE_BUILD}
 sed -i 's/@VERSION@/'${VERSION}'/g' ${GRADLE_BUILD}
 sed -i 's/@MAPPINGS@/'${MAPPINGS}'/g' ${GRADLE_BUILD}
 
-#gradle recompMinecraft
+#gradle recompileMC
